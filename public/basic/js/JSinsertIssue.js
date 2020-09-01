e38305f4c91cc0b15c6c0bfea4909de8a8d9d18a
ankit18-259 <ankit18_259@aec.ac.in>
2020-09-02 04:37:30 +0530

    skeleton completed

--- public/basic/js/JSinsertIssue.js
+++ public/basic/js/JSinsertIssue.js
@@ -1,3 +1,21 @@
-document.getElementById(Issue_1).innerHTML("Issue3");
-document.getElementById(Issue_2).innerHTML("Issue2")
-document.getElementById(Issue_3).innerHTML("Issue3")
+function createMenuItem(item, itemURL) {
+    let li = document.createElement('li');
+    let a = document.createElement('a')
+    li.appendChild(a);
+    a.setAttribute('href', itemURL);
+    a.innerHTML = item;
+    return li;
+    }
+document.getElementById('menu').appendChild(createMenuItem('Issue 1', '/issue1/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 2', '/issue2/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 3', '/issue3/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 4', '/issue4/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 5', '/issue5/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 6', '/issue6/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 7', '/issue7/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 8', '/issue8/1'));
+
+
+    
+
+
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require ('path');
 
 const app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 9000;
 
 
 app.use(express.static(path.join(__dirname + '/public')));
@@ -21,6 +21,68 @@ app.get('/issue1/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Is
 app.get('/issue1/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/7.html')));
 app.get('/issue1/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/8.html')));
 
+app.get('/issue2/1', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue2/1.html')));
+app.get('/issue2/2', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue2/2.html')));
+app.get('/issue2/3', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue2/3.html')));
+app.get('/issue2/4', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue2/4.html')));
+app.get('/issue2/5', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue2/5.html')));
+app.get('/issue2/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue2/6.html')));
+app.get('/issue2/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue2/7.html')));
+app.get('/issue2/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue2/8.html')));
 
+app.get('/issue3/1', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue3/1.html')));
+app.get('/issue3/2', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue3/2.html')));
+app.get('/issue3/3', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue3/3.html')));
+app.get('/issue3/4', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue3/4.html')));
+app.get('/issue3/5', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue3/5.html')));
+app.get('/issue3/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue3/6.html')));
+app.get('/issue3/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue3/7.html')));
+app.get('/issue3/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue3/8.html')));
+
+app.get('/issue4/1', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue4/1.html')));
+app.get('/issue4/2', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue4/2.html')));
+app.get('/issue4/3', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue4/3.html')));
+app.get('/issue4/4', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue4/4.html')));
+app.get('/issue4/5', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue4/5.html')));
+app.get('/issue4/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue4/6.html')));
+app.get('/issue4/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue4/7.html')));
+app.get('/issue4/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue4/8.html')));
+
+app.get('/issue5/1', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue5/1.html')));
+app.get('/issue5/2', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue5/2.html')));
+app.get('/issue5/3', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue5/3.html')));
+app.get('/issue5/4', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue5/4.html')));
+app.get('/issue5/5', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue5/5.html')));
+app.get('/issue5/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue5/6.html')));
+app.get('/issue5/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue5/7.html')));
+app.get('/issue5/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue5/8.html')));
+
+app.get('/issue6/1', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue6/1.html')));
+app.get('/issue6/2', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue6/2.html')));
+app.get('/issue6/3', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue6/3.html')));
+app.get('/issue6/4', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue6/4.html')));
+app.get('/issue6/5', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue6/5.html')));
+app.get('/issue6/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue6/6.html')));
+app.get('/issue6/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue6/7.html')));
+app.get('/issue6/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue6/8.html')));
+
+
+app.get('/issue7/1', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue7/1.html')));
+app.get('/issue7/2', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue7/2.html')));
+app.get('/issue7/3', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue7/3.html')));
+app.get('/issue7/4', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue7/4.html')));
+app.get('/issue7/5', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue7/5.html')));
+app.get('/issue7/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue7/6.html')));
+app.get('/issue7/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue7/7.html')));
+app.get('/issue7/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue7/8.html')));
+
+app.get('/issue8/1', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue8/1.html')));
+app.get('/issue8/2', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue8/2.html')));
+app.get('/issue8/3', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue8/3.html')));
+app.get('/issue8/4', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue8/4.html')));
+app.get('/issue8/5', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue8/5.html')));
+app.get('/issue8/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue8/6.html')));
+app.get('/issue8/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue8/7.html')));
+app.get('/issue8/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue8/8.html')));
 
 app.listen(PORT);
