531d166ef47bd312f1165ceffe41cd4c068c2df1
ankit18-259 <ankit18_259@aec.ac.in>
2020-09-01 14:36:20 +0530

    skeleton

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ var PORT = process.env.PORT || 8080;
 
 
 app.use(express.static(path.join(__dirname + '/public')));
+
 app.get('/', (req,res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/aboutus', (req,res) => res.sendFile(path.join(__dirname + '/public/aboutus.html')));
+
+app.get('/issue1/1', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/1.html')));
+app.get('/issue1/2', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/2.html')));
+app.get('/issue1/3', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/3.html')));
+app.get('/issue1/4', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/4.html')));
+app.get('/issue1/5', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/5.html')));
+app.get('/issue1/6', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/6.html')));
+app.get('/issue1/7', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/7.html')));
+app.get('/issue1/8', (req,res) => res.sendFile(path.join(__dirname + '/public/Issue1/8.html')));
+
+
 
 app.listen(PORT);
