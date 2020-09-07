de7ce2d62f2952c4acf9db8f088570c5f84997cb
ankit18-259 <ankit18_259@aec.ac.in>
2020-09-08 04:01:54 +0530

    design revamp

---
+++ public/basic/js/default.js
@@ -0,0 +1,46 @@
+/********************************************************************************
+ ********************************************************************************
+                           onload starts                           
+********************************************************************************
+********************************************************************************/
+
+// Drop Down Javascript
+function createMenuItem(item, itemURL) {
+    let li = document.createElement('li');
+    let a = document.createElement('a')
+    li.appendChild(a);
+    a.setAttribute('href', itemURL);
+    a.innerHTML = item;
+    return li;
+    }
+document.getElementById('menu').appendChild(createMenuItem('Issue 1', '/issue1/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 2', '/issue2/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 3', '/issue3/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 4', '/issue4/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 5', '/issue5/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 6', '/issue6/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 7', '/issue7/1'));
+document.getElementById('menu').appendChild(createMenuItem('Issue 8', '/issue8/1'));
+//Drop Down Javascript ends
+
+//general hacks onload starts
+
+document.getElementById("hamburgerIcon").style.cursor = "pointer";
+
+//general hacks onload ends
+
+/********************************************************************************
+ ********************************************************************************
+                           onload ends
+********************************************************************************
+********************************************************************************/
+
+
+function floaterLoad() {
+    document.write("Bhat dali");
+}
+
+
+    
+
+
